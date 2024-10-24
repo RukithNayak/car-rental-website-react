d236d0b0a45d4ec2f5344caacb42a294523fbd74
RukithNayak <rukith.nayak@gmail.com>
2024-10-24 21:30:27 +0530

    first update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Header from './components/Header/Header'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Info from './pages/Info/Info';
 
 const App = () => {
   return (
-    <div className = 'app'>
-      <Navbar/>
+    <div className='app'>
+      <Navbar />
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/order" element = {<PlaceOrder/>}/>
+        <Route path="/info" element={<Info />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets' 
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const[menu, setMenu] = useState("home");
-  return (
-    <div className = "navbar">
-        <img src= {assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <li onClick = {() => setMenu("home")} className = {menu === "home" ? "active" : ""}>Home</li>
-            <li onClick = {() => setMenu("info")} className = {menu === "info" ? "active" : ""}>Info</li>
-            <li onClick = {() => setMenu("services")} className = {menu === "services" ? "active" : ""}>Services</li>
-            <li onClick = {() => setMenu("contact-us")} className = {menu === "contact-us" ? "active" : ""}>Contact Us</li>
-        </ul>
-        <div className = "navbar-right">
-            <img src = {assets.search_icon}/>
-            <div className="navbar-search-icon">
-                <div className="dot"></div>
+    const [menu, setMenu] = useState("home");
+    return (
+        <div className="navbar">
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("info")} className={menu === "info" ? "active" : ""}>
+                    <Link to="/info">Info</Link>
+                </li>
+                <li onClick={() => setMenu("services")} className={menu === "services" ? "active" : ""}>Services</li>
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="search icon" />
+                <div className="navbar-search-icon">
+                    <div className="dot"></div>
+                </div>
+                <button>Sign in</button>
             </div>
-            <button>sign in</button>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
